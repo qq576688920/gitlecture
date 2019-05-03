485b205fedb33d9440bd01cae1229176aa8afc81
superPilipala <576688920@qq.com>
2019-05-04 02:50:55 +0800

    Initial Commit

---
+++ GitFirst/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  GitFirst
+//
+//  Created by 曾罗轩 on 2019/5/4.
+//  Copyright © 2019 曾罗轩. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
